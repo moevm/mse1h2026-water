dd955333994bfb2e459bc23bb6cb8dbd3bf60251
KonasovaYana <konasovay@gmail.com>
2026-03-13 20:39:20 +0300

    fix: change timeout

--- front/streamlit_app.py
+++ front/streamlit_app.py
@@ -124,7 +124,7 @@ def get_water_info_from_backend(coords: Coords) -> Optional[Dict[str, Any]]:
                 "lat": coords.lat,
                 "lon": coords.lon
             },
-            timeout=10  
+            timeout=60  
         )
         response.raise_for_status()
         return response.json()
