1755e4f100b3c98b5fbdf93f70d1dde307330bcc
Nikita Kozmin <kozmin2100@gmail.com>
2026-04-25 14:48:54 +0300

    check ci for changes

--- back/app.py
+++ back/app.py
@@ -53,6 +53,7 @@ async def get_water_info(
 
 app.include_router(router)
 app.include_router(methods.router)
+
 os.makedirs("img/classified", exist_ok=True)
 os.makedirs("geojson", exist_ok=True)
 app.mount("/img/classified", StaticFiles(directory="img/classified"), name="Классифицированные изображения")
